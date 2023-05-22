ddca85844a57b9f39a8a12f6e00c15ad86f0b01c
Sandro <20a3110125@untels.edu.pe>
2023-05-22 14:49:24 -0500

    EstructuraLista

---
+++ DAMEP_ErickP_ThaliaV_SandroW/app/src/main/java/com/example/damep_erickp_thaliav_sandrow/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.damep_erickp_thaliav_sandrow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
