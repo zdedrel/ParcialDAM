f17344652d03d872faccbf5826a4e7a1ba8153a5
zdedrel <20a3110196@untels.edu.pe>
2023-05-22 16:00:53 -0500

    ErickParillo

---
+++ DAMEP_ErickP_ThaliaV_SandroW/.idea/app/src/main/java/com/example/damep_erickp_thaliav_sandrow/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.damep_erickp_thaliav_sandrow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.buttonEnviar);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Se envió sus datos", Toast.LENGTH_SHORT).show());
+    }
+}
